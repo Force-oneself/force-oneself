b87cbc8b7a26eeea603cdaead5c814d526d1cc22
Force-oneself <1044749290@qq.com>
2020-04-24 00:00:07 +0800

    RabbitMQ工具类进行重构

--- src/main/java/com/quan/mq/rabbit/utils/RabbitMQUtils.java
+++ src/main/java/com/quan/mq/rabbit/utils/RabbitMQUtils.java
@@ -9,23 +9,37 @@ import java.util.concurrent.TimeoutException;
 
 public class RabbitMQUtils {
 
+    private final static String HOST_NAME;
+    private final static int PORT;
+    private final static String USER_NAME;
+    private final static String PASSWORD;
+    private final static String VIRTUAL_HOST;
+    private final static ConnectionFactory FACTORY;
+
+    static {
+        HOST_NAME = "127.0.0.1";
+        PORT = 5672;
+        USER_NAME = "guest";
+        PASSWORD = "guest";
+        VIRTUAL_HOST = "/";
+        FACTORY = new ConnectionFactory();
+        //设置服务端地址（域名地址/ip）
+        FACTORY.setHost(HOST_NAME);
+        //设置服务器端口号
+        FACTORY.setPort(PORT);
+        //设置虚拟主机(相当于数据库中的库)
+        FACTORY.setVirtualHost(VIRTUAL_HOST);
+        //设置用户名
+        FACTORY.setUsername(USER_NAME);
+        //设置密码
+        FACTORY.setPassword(PASSWORD);
+    }
+
     public static Connection getConnecion(){
         try
         {
             Connection connection = null;
-            //定义一个连接工厂
-            ConnectionFactory factory = new ConnectionFactory();
-            //设置服务端地址（域名地址/ip）
-            factory.setHost("127.0.0.1");
-            //设置服务器端口号
-            factory.setPort(5672);
-            //设置虚拟主机(相当于数据库中的库)
-            factory.setVirtualHost("/");
-            //设置用户名
-            factory.setUsername("guest");
-            //设置密码
-            factory.setPassword("guest");
-            connection = factory.newConnection();
+            connection = FACTORY.newConnection();
             return connection;
         }
         catch (Exception e)
@@ -51,6 +65,6 @@ public class RabbitMQUtils {
                 e.printStackTrace();
             }
         }
-
     }
+
 }
