62653be1a590cb7fa651069bf63a29a143945d43
Force-oneself <1044749290@qq.com>
2020-04-23 23:33:07 +0800

    使用RabbitMQ的第一个HelloWorld

---
+++ src/main/java/com/quan/mq/rabbit/Recv.java
@@ -0,0 +1,36 @@
+package com.quan.mq.rabbit;
+
+import com.quan.mq.rabbit.utils.RabbitMQUtils;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.io.IOException;
+
+public class Recv {
+    private final static String QUEUE_NAME = "hello";
+
+    public static void firstConsume() {
+        Connection connection = RabbitMQUtils.getConnecion();
+        Channel channel = null;
+
+        try {
+            channel = connection.createChannel();
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                String message = new String(delivery.getBody(), "UTF-8");
+                System.out.println(" [x] Received '" + message + "'");
+            };
+            channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Recv.firstConsume();
+    }
+}
---
+++ src/main/java/com/quan/mq/rabbit/utils/RabbitMQUtils.java
@@ -0,0 +1,56 @@
+package com.quan.mq.rabbit.utils;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class RabbitMQUtils {
+
+    public static Connection getConnecion(){
+        try
+        {
+            Connection connection = null;
+            //定义一个连接工厂
+            ConnectionFactory factory = new ConnectionFactory();
+            //设置服务端地址（域名地址/ip）
+            factory.setHost("127.0.0.1");
+            //设置服务器端口号
+            factory.setPort(5672);
+            //设置虚拟主机(相当于数据库中的库)
+            factory.setVirtualHost("/");
+            //设置用户名
+            factory.setUsername("guest");
+            //设置密码
+            factory.setPassword("guest");
+            connection = factory.newConnection();
+            return connection;
+        }
+        catch (Exception e)
+        {
+            return null;
+        }
+    }
+
+    public static void close(Connection connection, Channel channel) {
+        if (channel != null){
+            try {
+                channel.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (TimeoutException e) {
+                e.printStackTrace();
+            }
+        }
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
