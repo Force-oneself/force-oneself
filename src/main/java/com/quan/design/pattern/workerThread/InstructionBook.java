384168a3767c32fe29752da01444d6e2fc9b9a10
Force-oneself <1044749290@qq.com>
2020-07-02 10:53:09 +0800

    添加Worker-Thread设计模式

---
+++ src/main/java/com/quan/design/pattern/workerThread/InstructionBook.java
@@ -0,0 +1,16 @@
+package com.quan.design.pattern.workerThread;
+
+/**
+ * 在流水线上需要被加工的产品，create作为一个模板方法，提供了加工产品的说明书
+ */
+public abstract class InstructionBook {
+
+    public final void create() {
+        this.firstProcess();
+        this.secondProcess();
+    }
+
+    protected abstract void firstProcess();
+
+    protected abstract void secondProcess();
+}
