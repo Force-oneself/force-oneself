fa8a89f359ccb3c2d1fa26cf7dde9b32a1853bc0
Force-oneself <1044749290@qq.com>
2020-07-03 15:13:43 +0800

    添加多线程观察者（监控任务的生命周期）设计模式

---
+++ src/main/java/com/quan/design/pattern/thread/observer/ObservableThreadTest.java
@@ -0,0 +1,42 @@
+package com.quan.design.pattern.thread.observer;
+
+import java.util.concurrent.TimeUnit;
+
+public class ObservableThreadTest {
+
+    public static void useObservable() {
+        Observable observableThread = new ObservableThread<>(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(" finished done.");
+            return null;
+        });
+        observableThread.start();
+    }
+
+    public static void useTaskLifecycle() {
+        final TaskLifecycle<String> lifecycle = new TaskLifecycle.EmptyLifecycle<String>() {
+            public void onFinish(Thread thread, String result) {
+                System.out.println("The result is " + result);
+            }
+        };
+        Observable observableThread = new ObservableThread<>(lifecycle, () -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(" finished done.");
+            return "Hello Observer";
+        });
+        observableThread.start();
+    }
+
+    public static void main(String[] args) {
+        useObservable();
+        useTaskLifecycle();
+    }
+}
