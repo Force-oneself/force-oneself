b8a468d8e626fb012c5685e15cb396266014eb16
Force-oneself <1044749290@qq.com>
2020-07-04 20:37:42 +0800

    添加SingleExecution设计模式

---
+++ src/main/java/com/quan/design/pattern/thread/singleExecution/noodle/EatNoodleThread.java
@@ -0,0 +1,57 @@
+package com.quan.design.pattern.thread.singleExecution.noodle;
+
+public class EatNoodleThread extends Thread {
+
+    private final String name;
+
+    private final Tableware leftTool;
+
+    private final Tableware rightTool;
+
+    private final TablewarePair tablewarePair;
+
+    public EatNoodleThread(String name, Tableware leftTool, Tableware rightTool) {
+        this.name = name;
+        this.leftTool = leftTool;
+        this.rightTool = rightTool;
+        this.tablewarePair = null;
+    }
+
+    public EatNoodleThread(String name, TablewarePair tablewarePair) {
+        this.name = name;
+        this.tablewarePair = tablewarePair;
+        this.leftTool = null;
+        this.rightTool = null;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            this.eat();
+        }
+    }
+
+    // 吃面过程
+    private void eat() {
+        synchronized (leftTool) {
+            System.out.println(name + " take up " + leftTool + "(left)");
+            synchronized (rightTool) {
+                System.out.println(name + " take up " + rightTool + "(right)");
+                System.out.println(name + " is eating now.");
+                System.out.println(name + " put down " + rightTool + "(right)");
+            }
+            System.out.println(name + " put down " + rightTool + "(right)");
+        }
+    }
+
+    private void useTablewarePair() {
+        synchronized (tablewarePair) {
+            System.out.println(name + " take up " + tablewarePair.getLeftTool() + "(left)");
+            System.out.println(name + " take up " + tablewarePair.getRightTool() + "(right)");
+            System.out.println(name + " is eating now.");
+            System.out.println(name + " put down " + tablewarePair.getRightTool() + "(right)");
+            System.out.println(name + " take up " + tablewarePair.getLeftTool() + "(left)");
+        }
+    }
+
+}
---
+++ src/main/java/com/quan/design/pattern/thread/singleExecution/noodle/TablewarePair.java
@@ -0,0 +1,20 @@
+package com.quan.design.pattern.thread.singleExecution.noodle;
+
+public class TablewarePair {
+
+    private final Tableware leftTool;
+    private final Tableware rightTool;
+
+    public TablewarePair(Tableware leftTool, Tableware rightTool) {
+        this.leftTool = leftTool;
+        this.rightTool = rightTool;
+    }
+
+    public Tableware getLeftTool() {
+        return leftTool;
+    }
+
+    public Tableware getRightTool() {
+        return rightTool;
+    }
+}
