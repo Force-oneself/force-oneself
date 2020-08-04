b9ab254d9fe22662772dd1cc227f1455f61f40c0
Force-oneself <1044749290@qq.com>
2020-08-05 02:43:03 +0800

    添加算法和数据结构的实现

---
+++ src/main/java/com/quan/net/upload/Client.java
@@ -0,0 +1,46 @@
+package com.quan.net.upload;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class Client {
+
+    /**
+     * 实现步骤:
+     *         1.创建一个本地字节输入流FileInputStream对象,构造方法中绑定要读取的数据源
+     *         2.创建一个客户端Socket对象,构造方法中绑定服务器的IP地址和端口号
+     *         3.使用Socket中的方法getOutputStream,获取网络字节输出流OutputStream对象
+     *         4.使用本地字节输入流FileInputStream对象中的方法read,读取本地文件
+     *         5.使用网络字节输出流OutputStream对象中的方法write,把读取到的文件上传到服务器
+     *         6.使用Socket中的方法getInputStream,获取网络字节输入流InputStream对象
+     *         7.使用网络字节输入流InputStream对象中的方法read读取服务回写的数据
+     *         8.释放资源(FileInputStream,Socket)
+     *
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("D:\\Picture\\Desktop\\girl.jpg");
+
+        Socket socket = new Socket("127.0.0.1", 8888);
+
+        OutputStream outputStream = socket.getOutputStream();
+        int len = 0;
+        byte[] bytes = new byte[8192];
+        while ((len = fileInputStream.read(bytes)) != -1) {
+            outputStream.write(bytes, 0, len);
+        }
+
+        socket.shutdownOutput();
+
+        InputStream inputStream = socket.getInputStream();
+        while ((len = inputStream.read(bytes)) != -1) {
+            System.out.println(new String(bytes, 0, len));
+        }
+        fileInputStream.close();
+        socket.close();
+    }
+}
---
+++ src/main/java/com/quan/net/upload/Server.java
@@ -0,0 +1,47 @@
+package com.quan.net.upload;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    /**
+     * 实现步骤:
+     *         1.创建一个服务器ServerSocket对象,和系统要指定的端口号
+     *         2.使用ServerSocket对象中的方法accept,获取到请求的客户端Socket对象
+     *         3.使用Socket对象中的方法getInputStream,获取到网络字节输入流InputStream对象
+     *         4.判断d:\\upload文件夹是否存在,不存在则创建
+     *         5.创建一个本地字节输出流FileOutputStream对象,构造方法中绑定要输出的目的地
+     *         6.使用网络字节输入流InputStream对象中的方法read,读取客户端上传的文件
+     *         7.使用本地字节输出流FileOutputStream对象中的方法write,把读取到的文件保存到服务器的硬盘上
+     *         8.使用Socket对象中的方法getOutputStream,获取到网络字节输出流OutputStream对象
+     *         9.使用网络字节输出流OutputStream对象中的方法write,给客户端回写"上传成功"
+     *
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSocket = new ServerSocket(8888);
+        Socket socket = serverSocket.accept();
+        InputStream inputStream = socket.getInputStream();
+
+        File file = new File("D:\\EventBusTest");
+        if (!file.exists()) {
+            file.mkdir();
+        }
+
+        FileOutputStream fileOutputStream = new FileOutputStream(file + "\\girl-cp.jpg");
+        int len = 0;
+        byte[] bytes = new byte[8192];
+        while ((len = inputStream.read(bytes)) != -1) {
+            fileOutputStream.write(len);
+        }
+        OutputStream outputStream = socket.getOutputStream();
+        outputStream.write("上传成功".getBytes());
+        fileOutputStream.close();
+        socket.close();
+        serverSocket.close();
+    }
+}
