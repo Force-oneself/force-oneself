6cbcb2df6436dcba42e6f3b1c08dc14d3b68d9fb
Force-oneself <1044749290@qq.com>
2021-01-29 18:53:28 +0800

    升级为SpringBoot项目

---
+++ src/main/java/com/quan/SpringWaterApplication.java
@@ -0,0 +1,31 @@
+package com.quan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.neo4j.Neo4jDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.neo4j.Neo4jRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.elasticsearch.rest.RestClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ *
+ * @Description: SpringBoot 入口
+ * @Author heyq
+ * @Date 2021-01-29
+ **/
+@SpringBootApplication(exclude = {
+        ElasticsearchAutoConfiguration.class,
+        RestClientAutoConfiguration.class,
+        Neo4jRepositoriesAutoConfiguration.class,
+        Neo4jDataAutoConfiguration.class,
+        DataSourceAutoConfiguration.class
+})
+public class SpringWaterApplication {
+
+    public static void main(String[] args) {
+        // es版本对应报错问题
+        System.setProperty("es.set.netty.runtime.available.processors", "false");
+        SpringApplication.run(SpringWaterApplication.class, args);
+    }
+}
