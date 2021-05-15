ad780f2eb8f1b1083ecf7dc64136e651d708d865
ForceOneself <1044749290@qq.com>
2021-05-15 18:05:48 +0800

    '刷新gitignore'

---
+++ force-common/src/test/java/com/quan/common/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quan.common.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
