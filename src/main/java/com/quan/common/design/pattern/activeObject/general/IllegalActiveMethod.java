034d498bc2cfab4778ab0c1e698d9ec5a2f1b4e4
ForceOneself <1044749290@qq.com>
2021-05-16 16:32:59 +0800

    添加算法模块

--- src/main/java/com/quan/common/design/pattern/activeObject/general/IllegalActiveMethod.java
+++
@@ -1,8 +0,0 @@
-package com.quan.common.design.pattern.activeObject.general;
-
-public class IllegalActiveMethod extends Exception {
-
-    public IllegalActiveMethod(String message) {
-        super(message);
-    }
-}
