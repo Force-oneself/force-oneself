a016efadb3318cedd01e1d14f8cdd79da131aebb
ForceOneself <1044749290@qq.com>
2021-05-22 15:25:05 +0800

    update git

--- force-pattern/src/main/java/com/quan/pattern/multi/thread/event/driven/MessageMatcherException.java
+++
@@ -1,8 +0,0 @@
-package com.quan.pattern.multi.thread.event.driven;
-
-public class MessageMatcherException extends RuntimeException {
-
-    public MessageMatcherException(String message) {
-        super(message);
-    }
-}
