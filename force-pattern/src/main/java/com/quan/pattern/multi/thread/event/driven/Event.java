1f3dfb74b1f8cce70fd0626ab5ec152d4e87ea26
ForceOneself <1044749290@qq.com>
2021-05-22 14:49:47 +0800

    update gitignore

--- force-pattern/src/main/java/com/quan/pattern/multi/thread/event/driven/Event.java
+++
@@ -1,9 +0,0 @@
-package com.quan.pattern.multi.thread.event.driven;
-
-public class Event implements Message {
-
-    @Override
-    public Class<? extends Message> getType() {
-        return getClass();
-    }
-}
