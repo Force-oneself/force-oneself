d79454b05c93332c4b8cd1dfb7f1cc0e1bd0f7b8
Force-oneself <1044749290@qq.com>
2021-07-22 23:32:31 +0800

    add（security）：添加security module

---
+++ force-framework/force-security/src/main/java/com/quan/framework/security/ForceSecurityApplication.java
@@ -0,0 +1,17 @@
+package com.quan.framework.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Force-oneself
+ * @Description ForceSecurityApplication.java
+ * @date 2021-07-21
+ */
+@SpringBootApplication
+public class ForceSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForceSecurityApplication.class, args);
+    }
+}
