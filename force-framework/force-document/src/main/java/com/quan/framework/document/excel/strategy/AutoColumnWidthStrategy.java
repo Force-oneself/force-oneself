4ec50fbd77a37132256526ec96ad7c3aba9633db
Force-oneself <1044749290@qq.com>
2022-01-05 21:26:31 +0800

    :art: fix EasyExcel 自动列宽对数字对支持

--- force-framework/force-document/src/main/java/com/quan/framework/document/excel/strategy/AutoColumnWidthStrategy.java
+++ force-framework/force-document/src/main/java/com/quan/framework/document/excel/strategy/AutoColumnWidthStrategy.java
@@ -19,46 +19,48 @@ import java.util.Map;
  **/
 public class AutoColumnWidthStrategy extends AbstractColumnWidthStyleStrategy {
 
-    private Map<Integer, Map<Integer, Integer>> widthCache = new HashMap<>();
+    private final Map<Integer, Map<Integer, Integer>> widthCache = new HashMap<>(16);
 
     @Override
     protected void setColumnWidth(WriteSheetHolder writeSheetHolder, List<CellData> cellDataList, Cell cell, Head head,
                                   Integer relativeRowIndex, Boolean isHead) {
-        boolean needSetWidth = isHead || !CollectionUtils.isEmpty(cellDataList);
-        if (needSetWidth) {
-            Map<Integer, Integer> maxColumnWidthMap = widthCache.computeIfAbsent(writeSheetHolder.getSheetNo(), k -> new HashMap(16));
-            Integer columnWidth = this.dataLength(cellDataList, cell, isHead);
-            if (columnWidth >= 0) {
-                columnWidth = columnWidth <= 255 ? columnWidth : 255;
-                Integer maxColumnWidth = maxColumnWidthMap.get(cell.getColumnIndex());
-                if (maxColumnWidth == null || columnWidth > maxColumnWidth) {
-                    maxColumnWidthMap.put(cell.getColumnIndex(), columnWidth);
-                    writeSheetHolder.getSheet().setColumnWidth(cell.getColumnIndex(), columnWidth * 256);
-                }
-            }
+        boolean needSetWidth = (isHead != null && isHead) || !CollectionUtils.isEmpty(cellDataList);
+        if (!needSetWidth) {
+            return;
         }
+        Map<Integer, Integer> maxWidthMap = widthCache.computeIfAbsent(writeSheetHolder.getSheetNo(), k -> new HashMap<>(16));
+        Integer columnWidth = this.dataLength(cellDataList, cell, isHead);
+        if (columnWidth < 0) {
+            return;
+        }
+        columnWidth = Math.min(columnWidth, 255);
+        Integer maxColumnWidth = maxWidthMap.get(cell.getColumnIndex());
+        if (maxColumnWidth != null && columnWidth <= maxColumnWidth) {
+            return;
+        }
+        maxWidthMap.put(cell.getColumnIndex(), columnWidth);
+        writeSheetHolder.getSheet().setColumnWidth(cell.getColumnIndex(), columnWidth * 256);
     }
 
+
     private Integer dataLength(List<CellData> cellDataList, Cell cell, Boolean isHead) {
-        if (isHead) {
-            return cell.getStringCellValue().getBytes().length;
-        } else {
-            CellData cellData = cellDataList.get(0);
-            CellDataTypeEnum type = cellData.getType();
-            if (type == null) {
+        if (isHead != null && isHead) {
+            return Math.max(cell.getStringCellValue().getBytes().length, cell.getStringCellValue().length() * 2);
+        }
+        CellData cellData = cellDataList.get(0);
+        CellDataTypeEnum type = cellData.getType();
+        if (type == null) {
+            return -1;
+        }
+        switch (type) {
+            case STRING:
+                return cellData.getStringValue().getBytes().length;
+            case BOOLEAN:
+                return cellData.getBooleanValue().toString().getBytes().length;
+            case NUMBER:
+                return cellData.getNumberValue().toString().getBytes().length;
+            default:
                 return -1;
-            } else {
-                switch (type) {
-                    case STRING:
-                        return cellData.getStringValue().getBytes().length;
-                    case BOOLEAN:
-                        return cellData.getBooleanValue().toString().getBytes().length;
-                    case NUMBER:
-                        return cellData.getNumberValue().toString().getBytes().length;
-                    default:
-                        return -1;
-                }
-            }
         }
     }
 }
