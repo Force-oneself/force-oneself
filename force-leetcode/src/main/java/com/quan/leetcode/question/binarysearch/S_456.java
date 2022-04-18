d281344f9f4ec27b7b68606fd36e438b857c933a
Force-oneself <1044749290@qq.com>
2022-04-18 22:05:55 +0800

    :art: today's leetcode

---
+++ force-leetcode/src/main/java/com/quan/leetcode/question/binarysearch/S_456.java
@@ -0,0 +1,47 @@
+package com.quan.leetcode.question.binarysearch;
+
+import java.util.TreeMap;
+
+/**
+ * @author Force-oneself
+ * @description S_456
+ * @date 2022-04-18
+ */
+public class S_456 {
+
+    public boolean find132pattern(int[] nums) {
+        int n = nums.length;
+        if (n < 3) {
+            return false;
+        }
+
+        // 左侧最小值
+        int leftMin = nums[0];
+        // 右侧所有元素
+        TreeMap<Integer, Integer> rightAll = new TreeMap<>();
+
+        for (int k = 2; k < n; ++k) {
+            rightAll.put(nums[k], rightAll.getOrDefault(nums[k], 0) + 1);
+        }
+
+        for (int j = 1; j < n - 1; ++j) {
+            if (leftMin < nums[j]) {
+                // 返回小于等于该数的最小值，+1 为了恰好大于该值
+                Integer next = rightAll.ceilingKey(leftMin + 1);
+                // 存在大于最小值元素存在并小于中间的值
+                if (next != null && next < nums[j]) {
+                    return true;
+                }
+            }
+            // 让较小的加入
+            leftMin = Math.min(leftMin, nums[j]);
+            // 将右边的栈中去掉下一个需要遍历的数
+            rightAll.put(nums[j + 1], rightAll.get(nums[j + 1]) - 1);
+            if (rightAll.get(nums[j + 1]) == 0) {
+                rightAll.remove(nums[j + 1]);
+            }
+        }
+        return false;
+    }
+
+}
---
+++ force-leetcode/src/main/java/com/quan/leetcode/question/binarysearch/S_475.java
@@ -0,0 +1,80 @@
+package com.quan.leetcode.question.binarysearch;
+
+import java.util.Arrays;
+
+/**
+ * @author Force-oneself
+ * @description S_475
+ * @date 2022-04-18
+ */
+public class S_475 {
+
+
+    /**
+     * 排序+二分
+     *
+     * @param houses  houses
+     * @param heaters heaters
+     * @return return
+     */
+    public int findRadius1(int[] houses, int[] heaters) {
+        Arrays.sort(houses);
+        Arrays.sort(heaters);
+        int ans = 0;
+        for (int i = 0, j = 0; i < houses.length; i++) {
+            int curDistance = Math.abs(houses[i] - heaters[j]);
+            while (j < heaters.length - 1
+                    && Math.abs(houses[i] - heaters[j]) >= Math.abs(houses[i] - heaters[j + 1])) {
+                j++;
+                curDistance = Math.min(curDistance, Math.abs(houses[i] - heaters[j]));
+            }
+            ans = Math.max(ans, curDistance);
+        }
+        return ans;
+    }
+
+
+    /**
+     * 二分
+     *
+     * @param houses  houses
+     * @param heaters heaters
+     * @return return
+     */
+    public int findRadius(int[] houses, int[] heaters) {
+        int ans = 0;
+        Arrays.sort(heaters);
+        for (int house : houses) {
+            // 左边的取暖器
+            int i = binarySearch(heaters, house);
+            // 右边的取暖器
+            int j = i + 1;
+            // 左边间隔
+            int leftDistance = i < 0 ? Integer.MAX_VALUE : house - heaters[i];
+            // 右边的间隔
+            int rightDistance = j >= heaters.length ? Integer.MAX_VALUE : heaters[j] - house;
+            // 取小的间隔即可满足
+            int curDistance = Math.min(leftDistance, rightDistance);
+            // 全局需要取最大的才可以满足全局
+            ans = Math.max(ans, curDistance);
+        }
+        return ans;
+    }
+
+    public int binarySearch(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        // houses[0] > target 说明取右边的取暖器
+        if (nums[left] > target) {
+            return -1;
+        }
+        while (left < right) {
+            int mid = (right - left + 1) / 2 + left;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid;
+            }
+        }
+        return left;
+    }
+}
